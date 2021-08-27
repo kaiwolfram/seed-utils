4659ec0f742e9ef58df0bb7cc193fe28445656b1
kaiwitt <kaiwitt@protonmail.com>
2021-08-27 19:55:52 +0200

    Handle errors

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -1,16 +1,21 @@
 use clap::{App, Arg, ArgMatches};
-use lib::{derive_child_seed, derive_xpubs_from_seed, extend_seed, truncate_seed, xor_seeds};
+use lib::{
+    derive_child_seeds, derive_master_xpub, derive_xpubs_from_seed, extend_seed, truncate_seed,
+    xor_seeds,
+};
 
 const CHILD_SUB: &str = "child";
 const EXTEND_SUB: &str = "extend";
 const TRUNCATE_SUB: &str = "truncate";
 const XOR_SUB: &str = "xor";
+const XPRV_SUB: &str = "xprv";
 const XPUB_SUB: &str = "xpub";
 
-// TODO: Remove unwraps
-// TODO: Proper error handling
+// TODO: Docs
+// TODO: Readme.md
+// TODO: xprv
 
-fn main() -> Result<(), ()> {
+fn main() -> Result<(), String> {
     let matches = App::new("seed-utils")
         .version("0.1.0")
         .about("CLI seed utilities")
@@ -27,31 +32,52 @@ fn main() -> Result<(), ()> {
                 .arg(
                     Arg::with_name("index")
                         .help("Index to derive at.")
-                        .index(2)
-                        .required(true),
+                        .short("i")
+                        .long("index")
+                        .takes_value(true)
+                        .default_value("0"),
+                )
+                .arg(
+                    Arg::with_name("number")
+                        .help("Number of seeds to derive, starting from index.")
+                        .short("n")
+                        .long("number")
+                        .takes_value(true)
+                        .default_value("1"),
                 )
                 .arg(
                     Arg::with_name("word count")
                         .help("Number of words of the derived seed.")
                         .short("w")
                         .long("words")
+                        .takes_value(true)
+                        .possible_values(&["12", "18", "24"])
                         .default_value("24"),
                 ),
         )
         .subcommand(
             App::new(EXTEND_SUB)
-                .about("Creates a new seeds by extending the entropy of a 12 or 16 word seed")
+                .about("Creates a new seed by extending the entropy of a 12 or 18 word seed")
                 .arg(
                     Arg::with_name("seed")
                         .help("Seed to extend.")
                         .index(1)
                         .required(true),
                 )
+                .arg(
+                    Arg::with_name("word count")
+                        .help("Number of words of the extended seed.")
+                        .short("w")
+                        .long("words")
+                        .takes_value(true)
+                        .possible_values(&["18", "24"])
+                        .default_value("24"),
+                ),
         )
         .subcommand(
             App::new(TRUNCATE_SUB)
-                .about("Creates new seeds by shortening the entropy of another seed. 
-                The new seed begins with the same words as the longer seed, only the last one is different to satisfy the checksum.")
+                .about("Creates new seeds by shortening the entropy of another. 
+                The new seed begins with the same words as the longer one, only the last word is different to satisfy its checksum.")
                 .arg(
                     Arg::with_name("seed")
                         .help("Seed to truncate.")
@@ -61,13 +87,14 @@ fn main() -> Result<(), ()> {
         )
         .subcommand(
             App::new(XOR_SUB)
-            .about("XORs two seeds.")
+            .about("Does a XOR of multiple seeds.")
             .arg(
                 Arg::with_name("seed")
                     .help("Seeds to xor.")
                     .short("s")
                     .long("seed")
                     .multiple(true)
+                    .min_values(2)
                     .required(true),
             ),
         )
@@ -81,24 +108,75 @@ fn main() -> Result<(), ()> {
                         .required(true),
                 )
                 .arg(
+                    Arg::with_name("master")
+                        .help("Derive master xpub.")
+                        .long("master")
+                        .takes_value(false)
+                        .conflicts_with_all(&["index", "number"]),
+                )
+                .arg(
                     Arg::with_name("index")
                         .help("Index to derive xpub at.")
                         .short("i")
-                        .long("index"),
+                        .long("index")
+                        .takes_value(true)
+                        .default_value("0"),
                 )
+                .arg(
+                    Arg::with_name("number")
+                        .help("Number of xpubs to derive, starting from index.")
+                        .short("n")
+                        .long("number")
+                        .takes_value(true)
+                        .default_value("1"),
+                ),
+        )
+        .subcommand(
+            App::new(XPRV_SUB)
+                .about("Derives account xprvs from a seed.")
+                .arg(
+                    Arg::with_name("seed")
+                        .help("Seed to derive xprvs from.")
+                        .index(1)
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("master")
+                        .help("Derive master xprv.")
+                        .long("master")
+                        .takes_value(false)
+                        .conflicts_with_all(&["index", "number"]),
+                )
+                .arg(
+                    Arg::with_name("index")
+                        .help("Index to derive xprv at.")
+                        .short("i")
+                        .long("index")
+                        .takes_value(true)
+                        .default_value("0"),
+                )
+                .arg(
+                    Arg::with_name("number")
+                        .help("Number of xprvs to derive, starting from index.")
+                        .short("n")
+                        .long("number")
+                        .takes_value(true)
+                        .default_value("1"),
+                ),
         )
         .get_matches();
     process_matches(&matches)
 }
 
 /// Processes command line arguments.
-fn process_matches(matches: &ArgMatches) -> Result<(), ()> {
+fn process_matches(matches: &ArgMatches) -> Result<(), String> {
     match matches.subcommand_name() {
         Some(CHILD_SUB) => process_child_matches(matches)?,
         Some(EXTEND_SUB) => process_extend_matches(matches)?,
         Some(TRUNCATE_SUB) => process_truncate_matches(matches)?,
         Some(XOR_SUB) => process_xor_matches(matches)?,
         Some(XPUB_SUB) => process_xpub_matches(matches)?,
+        Some(XPRV_SUB) => process_xprv_matches(matches)?,
         Some(unknown) => println!("Subcommand [{}] does not exist", unknown),
         None => println!("No subcommand was used"),
     }
@@ -106,89 +184,116 @@ fn process_matches(matches: &ArgMatches) -> Result<(), ()> {
     Ok(())
 }
 
-fn index_value(matches: &ArgMatches) -> Result<u32, ()> {
+fn index_value(matches: &ArgMatches) -> Result<u32, String> {
     matches
         .value_of("index")
-        .ok_or(())?
+        .ok_or("index not set".to_string())?
         .parse::<u32>()
-        .or_else(|_| Err(()))
+        .map_err(|_| "index can't be higher than 2^32".to_string())
 }
 
-fn seed_value<'a>(matches: &'a ArgMatches) -> Result<&'a str, ()> {
-    matches.value_of("seed").ok_or(())
+fn number_value(matches: &ArgMatches) -> Result<u8, String> {
+    matches
+        .value_of("number")
+        .ok_or("number not set".to_string())?
+        .parse::<u8>()
+        .map_err(|_| "number can't be higher than 255".to_string())
 }
 
-fn seed_values<'a>(matches: &'a ArgMatches) -> Result<Vec<&'a str>, ()> {
-    Ok(matches.values_of("seed").ok_or(())?.into_iter().collect())
+fn seed_value<'a>(matches: &'a ArgMatches) -> Result<&'a str, String> {
+    matches.value_of("seed").ok_or("seed not set".to_string())
 }
 
-fn word_count_value(matches: &ArgMatches) -> Result<u32, ()> {
+fn seed_values<'a>(matches: &'a ArgMatches) -> Result<Vec<&'a str>, String> {
+    Ok(matches
+        .values_of("seed")
+        .ok_or("seeds not set".to_string())?
+        .into_iter()
+        .collect())
+}
+
+fn word_count_value(matches: &ArgMatches) -> Result<u8, String> {
     matches
         .value_of("word count")
-        .ok_or(())?
-        .parse::<u32>()
-        .or_else(|_| Err(()))
+        .ok_or("word count not set")?
+        .parse::<u8>()
+        .map_err(|_| "word count can't be higher than 24".to_string())
 }
 
-fn process_child_matches(matches: &ArgMatches) -> Result<(), ()> {
+fn is_master(matches: &ArgMatches) -> bool {
+    matches.is_present("master")
+}
+
+fn process_child_matches(matches: &ArgMatches) -> Result<(), String> {
+    // Return early because every field is either required or has a default value
+    let seed_str = seed_value(matches)?;
+    let index = index_value(matches)?;
+    let number = number_value(matches)?;
+    let word_count = word_count_value(matches)?;
+
+    let derived = derive_child_seeds(seed_str, (index, index + number as u32), word_count)?;
+
+    for (i, mnemonic) in derived {
+        println!("Derived seed at {}: {}", i, mnemonic);
+    }
+
+    Ok(())
+}
+
+fn process_extend_matches(matches: &ArgMatches) -> Result<(), String> {
+    // Return early because every field is either required or has a default value
     let seed_str = seed_value(matches)?;
     let word_count = word_count_value(matches)?;
-    let index = index_value(matches)?;
 
-    let derived = derive_child_seed(seed_str, index, word_count);
-
-    println!("Derived seed at {}: {}", index, derived);
+    let extended_seed = extend_seed(seed_str, word_count)?;
+    println!("Extended seed: {}", extended_seed);
 
     Ok(())
 }
 
-fn process_extend_matches(matches: &ArgMatches) -> Result<(), ()> {
+fn process_truncate_matches(matches: &ArgMatches) -> Result<(), String> {
+    // Return early because seed is required
     let seed_str = seed_value(matches)?;
 
-    let extended_seeds = extend_seed(seed_str);
-    // TODO: print different word counts
-    for seed in extended_seeds {
-        println!("Extended seed: {}", seed);
+    let truncated_seeds = truncate_seed(&seed_str)?;
+
+    for (i, seed) in truncated_seeds {
+        println!("Truncated {} word seed: {}", i, seed);
     }
 
     Ok(())
 }
 
-fn process_truncate_matches(matches: &ArgMatches) -> Result<(), ()> {
-    let seed_str = seed_value(matches)?;
-
-    let truncated_seeds = truncate_seed(&seed_str);
-
-    for seed in truncated_seeds {
-        // TODO: Print different word lengths
-        println!("Truncated seed: {}", seed);
-    }
-
-    Ok(())
-}
-
-fn process_xor_matches(matches: &ArgMatches) -> Result<(), ()> {
+fn process_xor_matches(matches: &ArgMatches) -> Result<(), String> {
     let seeds = seed_values(matches)?;
-    let xor_seed = xor_seeds(&seeds);
+    let xor_seed = xor_seeds(&seeds)?;
 
     println!("XORed seed: {}", xor_seed);
 
     Ok(())
 }
 
-// TODO: Derive master xpub
-// TODO: Derive xpubs of different types
-// TODO: Derive xpubs of index ranges
-
-fn process_xpub_matches(matches: &ArgMatches) -> Result<(), ()> {
+fn process_xpub_matches(matches: &ArgMatches) -> Result<(), String> {
+    // Return early because every field is either required or has a default value
     let seed_str = seed_value(matches)?;
+    if is_master(matches) {
+        let master = derive_master_xpub(seed_str)?;
+        println!("Master xpub: {}", master);
+
+        return Ok(());
+    }
     let index = index_value(matches)?;
+    let number = number_value(matches)?;
 
-    let derived = derive_xpubs_from_seed(seed_str, (index, index));
+    let derived = derive_xpubs_from_seed(seed_str, (index, index + number as u32))?;
 
-    for xpub in derived {
-        println!("Derived xpub at {}: {}", index, xpub);
+    for (i, xpub) in derived {
+        println!("Derived xpub at {}: {}", i, xpub);
     }
 
     Ok(())
+}
+
+fn process_xprv_matches(matches: &ArgMatches) -> Result<(), String> {
+    todo!();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,97 +1,159 @@
 use std::str::FromStr;
 
-use bip85::bitcoin::util::bip32::{ChildNumber, DerivationPath, ExtendedPubKey};
+use bip85::bitcoin::secp256k1::Secp256k1;
+use bip85::bitcoin::Network;
+use bip85::{
+    bip39::Mnemonic,
+    bitcoin::util::bip32::{ChildNumber, DerivationPath, ExtendedPrivKey, ExtendedPubKey},
+};
 use rand::{thread_rng, Rng};
 
-const MAX_ENTROPY_BYTES: usize = 32;
-const MIN_ENTROPY_BYTES: usize = 16;
+const ENTROPY_24_WORDS_BYTES: usize = 32;
+const ENTROPY_18_WORDS_BYTES: usize = 24;
+const ENTROPY_12_WORDS_BYTES: usize = 16;
 
-pub fn derive_child_seed<S>(seed: S, index: u32, word_count: u32) -> String
+pub fn derive_child_seeds<S>(
+    seed: S,
+    (start, mut end): (u32, u32),
+    word_count: u8,
+) -> Result<Vec<(u32, Mnemonic)>, String>
 where
     S: AsRef<str>,
 {
-    let parsed_seed = bip85::bip39::Mnemonic::from_str(seed.as_ref()).unwrap();
-    let entropy = parsed_seed.to_entropy();
-    let xprv = bip85::bitcoin::util::bip32::ExtendedPrivKey::new_master(
-        bip85::bitcoin::Network::Bitcoin,
-        &entropy,
-    )
-    .unwrap();
-
-    let secp = bip85::bitcoin::secp256k1::Secp256k1::new();
-    let derived = bip85::to_mnemonic(&secp, &xprv, word_count, index).unwrap();
-
-    derived.to_string()
-}
-
-pub fn extend_seed<S>(seed: S) -> Vec<String>
-where
-    S: AsRef<str>,
-{
-    let parsed_seed = bip85::bip39::Mnemonic::from_str(seed.as_ref()).unwrap();
-    let mut entropy = parsed_seed.to_entropy();
-
-    if entropy.len() < MAX_ENTROPY_BYTES {
-        let mut rand = thread_rng();
-        let more_entropy = std::iter::repeat(())
-            .map(|()| rand.gen::<u8>())
-            .take(MAX_ENTROPY_BYTES - entropy.len());
-        entropy.extend(more_entropy);
+    if end < start {
+        end = start;
     }
-    let extended_seed = bip85::bip39::Mnemonic::from_entropy(&entropy).unwrap();
-
-    let mut result: Vec<String> = Vec::new();
-    result.push(extended_seed.to_string());
-
-    result
-}
-
-pub fn truncate_seed<S>(seed: S) -> Vec<String>
-where
-    S: AsRef<str>,
-{
-    let parsed_seed = bip85::bip39::Mnemonic::from_str(seed.as_ref()).unwrap();
-    let mut entropy = parsed_seed.to_entropy();
-    entropy.truncate(MIN_ENTROPY_BYTES);
-
-    let truncated_seed = bip85::bip39::Mnemonic::from_entropy(&entropy).unwrap();
-
-    let mut result: Vec<String> = Vec::new();
-    result.push(truncated_seed.to_string());
-
-    result
-}
-
-pub fn xor_seeds(seeds: &[&str]) -> String {
-    seeds
-        .iter()
-        .map(|seed| seed_xor::Mnemonic::from_str(seed).unwrap())
-        .reduce(|a, b| a ^ b)
-        .unwrap()
-        .to_string()
-}
-
-pub fn derive_xpubs_from_seed<S>(seed: S, index_range: (u32, u32)) -> Vec<String>
-where
-    S: AsRef<str>,
-{
-    let parsed_seed = bip85::bip39::Mnemonic::from_str(seed.as_ref()).unwrap();
+    let parsed_seed = parse_seed(seed)?;
     let entropy = parsed_seed.to_entropy();
-    let xprv = bip85::bitcoin::util::bip32::ExtendedPrivKey::new_master(
-        bip85::bitcoin::Network::Bitcoin,
-        &entropy,
-    )
-    .unwrap();
-
+    let xprv =
+        ExtendedPrivKey::new_master(Network::Bitcoin, &entropy).map_err(|e| e.to_string())?;
     let secp = bip85::bitcoin::secp256k1::Secp256k1::new();
-    let path = DerivationPath::from_str("m/84'/0'")
-        .unwrap()
-        .child(ChildNumber::from_normal_idx(index_range.0).unwrap());
-    let xpub = ExtendedPubKey::from_private(&secp, &xprv);
-    let derived = xpub.derive_pub(&secp, &path).unwrap();
 
-    let mut result: Vec<String> = Vec::new();
-    result.push(derived.to_string());
+    let mut result: Vec<(u32, Mnemonic)> = Vec::with_capacity(end as usize - start as usize);
 
-    result
+    for i in start..end {
+        let mnemonic =
+            bip85::to_mnemonic(&secp, &xprv, word_count as u32, i).map_err(|e| e.to_string())?;
+        result.push((i, mnemonic));
+    }
+
+    Ok(result)
+}
+
+pub fn extend_seed<S>(seed: S, word_count: u8) -> Result<Mnemonic, String>
+where
+    S: AsRef<str>,
+{
+    let parsed_seed = parse_seed(seed)?;
+    if parsed_seed.word_count() >= word_count as usize {
+        return Err("Seed is already longer or the same as the desired length".to_string());
+    }
+    let mut entropy = parsed_seed.to_entropy();
+    let mut rand = thread_rng();
+    let new_entropy_count = match word_count {
+        18 => ENTROPY_18_WORDS_BYTES - entropy.len(),
+        24 => ENTROPY_24_WORDS_BYTES - entropy.len(),
+        _ => return Err(format!("{} is not a valid word count", word_count)),
+    };
+
+    let more_entropy = std::iter::repeat(())
+        .map(|()| rand.gen::<u8>())
+        .take(new_entropy_count);
+    entropy.extend(more_entropy);
+
+    Mnemonic::from_entropy(&entropy).map_err(|e| e.to_string())
+}
+
+// TODO: with word_count
+pub fn truncate_seed<S>(seed: S) -> Result<Vec<(u8, Mnemonic)>, String>
+where
+    S: AsRef<str>,
+{
+    let parsed_seed = parse_seed(seed)?;
+    let mut entropy = parsed_seed.to_entropy();
+    let mut result: Vec<(u8, Mnemonic)> = Vec::with_capacity(2);
+
+    match parsed_seed.word_count() {
+        12 => return Ok(result),
+        18 | 24 => {
+            entropy.truncate(ENTROPY_12_WORDS_BYTES);
+            let truncated_12 = Mnemonic::from_entropy(&entropy).map_err(|e| e.to_string())?;
+            result.push((12, truncated_12));
+
+            if parsed_seed.word_count() == 24 {
+                entropy.truncate(ENTROPY_12_WORDS_BYTES);
+                let truncated_18 = Mnemonic::from_entropy(&entropy).map_err(|e| e.to_string())?;
+                result.push((18, truncated_18));
+            }
+        }
+        _ => {
+            return Err(format!(
+                "Truncation for {} word seeds is not implemented",
+                parsed_seed.word_count()
+            ))
+        }
+    }
+
+    Ok(result)
+}
+
+pub fn xor_seeds(seeds: &[&str]) -> Result<seed_xor::Mnemonic, String> {
+    let mut mnemonics: Vec<seed_xor::Mnemonic> = Vec::with_capacity(seeds.len());
+    for seed in seeds {
+        let mnemonic = seed_xor::Mnemonic::from_str(seed).map_err(|e| e.to_string())?;
+        mnemonics.push(mnemonic);
+    }
+
+    mnemonics
+        .into_iter()
+        .reduce(|a, b| a ^ b)
+        .ok_or_else(|| "No seeds to xor".to_string())
+}
+
+pub fn derive_xpubs_from_seed<S>(
+    seed: S,
+    (start, mut end): (u32, u32),
+) -> Result<Vec<(u32, ExtendedPubKey)>, String>
+where
+    S: AsRef<str>,
+{
+    if end < start {
+        end = start;
+    }
+    let secp = Secp256k1::new();
+    let master = derive_master_xpub(seed)?;
+    let path = DerivationPath::from_str("m/84'/0'").map_err(|e| e.to_string())?;
+    let mut result: Vec<(u32, ExtendedPubKey)> = Vec::with_capacity(end as usize - start as usize);
+
+    for i in start..end {
+        let child = ChildNumber::from_normal_idx(i).map_err(|e| e.to_string())?;
+        let child_path = path.child(child);
+        let derived = master
+            .derive_pub(&secp, &child_path)
+            .map_err(|e| e.to_string())?;
+        result.push((i, derived));
+    }
+
+    Ok(result)
+}
+
+pub fn derive_master_xpub<S>(seed: S) -> Result<ExtendedPubKey, String>
+where
+    S: AsRef<str>,
+{
+    let parsed_seed = parse_seed(seed)?;
+    let entropy = parsed_seed.to_entropy();
+    let xprv =
+        ExtendedPrivKey::new_master(Network::Bitcoin, &entropy).map_err(|e| e.to_string())?;
+
+    let secp = Secp256k1::new();
+
+    Ok(ExtendedPubKey::from_private(&secp, &xprv))
+}
+
+fn parse_seed<S>(seed: S) -> Result<Mnemonic, String>
+where
+    S: AsRef<str>,
+{
+    Mnemonic::from_str(seed.as_ref()).map_err(|e| e.to_string())
 }
